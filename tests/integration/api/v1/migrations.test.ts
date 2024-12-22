fd83eac6c88e4fd448608d2be64b8ff3d07243b2
Ramon Kroetz <ramonkroetz@gmail.com>
2024-12-22 23:38:35 +0000

    test: rename all descriptions to use new pattern and adding a new migration test

--- tests/integration/api/v1/migrations.test.ts
+++ tests/integration/api/v1/migrations.test.ts
@@ -8,35 +8,56 @@ beforeAll(async () => {
   await query('drop schema public cascade; create schema public;')
 })
 
-test('GET to /api/v1/migrations should return 200', async () => {
-  const { data, status, error } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations')
+describe('GET /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    test('Running pending migrations', async () => {
+      const { data, status, error } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations')
 
-  expect(status).toBe(200)
-  expect(error).toBe(null)
-  expect(Array.isArray(data)).toBe(true)
-  expect(data?.length).toBeGreaterThan(0)
+      expect(status).toBe(200)
+      expect(error).toBe(null)
+      expect(Array.isArray(data)).toBe(true)
+      expect(data?.length).toBeGreaterThan(0)
+    })
+  })
 })
 
-test('POST to /api/v1/migrations should return 200', async () => {
-  const { data, status, error } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations', {
-    method: 'POST',
+describe('POST /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    describe('Running pending migrations', () => {
+      test('For the first time', async () => {
+        const { data, status, error } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations', {
+          method: 'POST',
+        })
+
+        expect(status).toBe(201)
+        expect(error).toBe(null)
+        expect(Array.isArray(data)).toBe(true)
+        expect(data?.length).toBeGreaterThan(0)
+      })
+
+      test('For the second time', async () => {
+        const { data, status, error } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations', {
+          method: 'POST',
+        })
+
+        expect(status).toBe(200)
+        expect(error).toBe(null)
+        expect(Array.isArray(data)).toBe(true)
+        expect(data?.length).toBe(0)
+      })
+    })
   })
+})
 
-  expect(status).toBe(201)
-  expect(error).toBe(null)
-  expect(Array.isArray(data)).toBe(true)
-  expect(data?.length).toBeGreaterThan(0)
+describe('PUT /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    test('Running pending migrations', async () => {
+      const { status, error } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations', {
+        method: 'PUT',
+      })
 
-  const {
-    data: data2,
-    status: status2,
-    error: error2,
-  } = await api<RunMigration[]>('http://localhost:3000/api/v1/migrations', {
-    method: 'POST',
+      expect(status).toBe(405)
+      expect(error).toBe('Method PUT not allowed')
+    })
   })
-
-  expect(status2).toBe(200)
-  expect(error2).toBe(null)
-  expect(Array.isArray(data2)).toBe(true)
-  expect(data2?.length).toBe(0)
 })
