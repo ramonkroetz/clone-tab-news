f9c5c6df0a04e99e7ca2483745ef02e906c70263
Ramon Kroetz <ramonkroetz@gmail.com>
2024-12-29 01:34:30 +0000

    feat: create InternalServerError and use in /api/v1/status

--- pages/api/v1/status.ts
+++ pages/api/v1/status.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { query } from 'infra/database'
+import { InternalServerError } from 'infra/errors'
 
 export type StatusResponse = {
   update_at: string
@@ -13,28 +14,35 @@ export type StatusResponse = {
 }
 
 export default async function status(_: NextApiRequest, response: NextApiResponse) {
-  const udpateAt = new Date().toISOString()
-  const databaseVersionResult = await query<{ server_version: string }>('SHOW server_version;')
-  const databaseMaxConnectionResult = await query<{ max_connections: string }>('SHOW max_connections;')
-  const databaseOpenedConnectionsResult = await query<{ opened_connections: number }>({
-    text: `
-      SELECT COUNT(*)::int AS opened_connections
-      FROM pg_stat_activity
-      WHERE datname = $1;
-    `,
-    values: [process.env.POSTGRES_DB],
-  })
+  try {
+    const udpateAt = new Date().toISOString()
+    const databaseVersionResult = await query<{ server_version: string }>('SHOW server_version;')
+    const databaseMaxConnectionResult = await query<{ max_connections: string }>('SHOW max_connections;')
+    const databaseOpenedConnectionsResult = await query<{ opened_connections: number }>({
+      text: `
+        SELECT COUNT(*)::int AS opened_connections
+        FROM pg_stat_activity
+        WHERE datname = $1;
+      `,
+      values: [process.env.POSTGRES_DB],
+    })
 
-  const statusData: StatusResponse = {
-    update_at: udpateAt,
-    dependencies: {
-      database: {
-        opened_connections: databaseOpenedConnectionsResult.rows[0].opened_connections,
-        max_connections: Number(databaseMaxConnectionResult.rows[0].max_connections),
-        version: databaseVersionResult.rows[0].server_version,
+    const statusData: StatusResponse = {
+      update_at: udpateAt,
+      dependencies: {
+        database: {
+          opened_connections: databaseOpenedConnectionsResult.rows[0].opened_connections,
+          max_connections: Number(databaseMaxConnectionResult.rows[0].max_connections),
+          version: databaseVersionResult.rows[0].server_version,
+        },
       },
-    },
-  }
+    }
 
-  response.status(200).json(statusData)
+    response.status(200).json(statusData)
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    })
+    response.status(500).json(publicErrorObject)
+  }
 }
